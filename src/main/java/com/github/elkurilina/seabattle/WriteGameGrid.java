fa5c2881d3d6878ec0840ff6e934d594407be4bd
Elena Kurilina <elkurilina@gmail.com>
2014-08-11 12:19:31 +0200

    Mark ship as dead after last ship part was hit

--- src/main/java/com/github/elkurilina/seabattle/WriteGameGrid.java
+++ src/main/java/com/github/elkurilina/seabattle/WriteGameGrid.java
@@ -1,5 +1,9 @@
 package com.github.elkurilina.seabattle;
 
+import java.awt.*;
+import java.util.Collection;
+import java.util.HashSet;
+
 /**
  * @author Elena Kurilina
  */
@@ -7,40 +11,68 @@ public class WriteGameGrid extends MaskedGameGrid {
 
     public final MaskedGameGrid maskedGrid;
 
-    public WriteGameGrid(Iterable<Cell> shipLocation, int gridSize) {
+    public WriteGameGrid(Collection<Cell> shipLocation, int gridSize) {
         super(shipLocation, gridSize);
         maskedGrid = new MaskedGameGrid(grid);
     }
 
     public CellState getCellState(Cell cell) {
-        return super.getCellValue(cell);
+        return super.getCellOpenState(cell);
     }
 
     public ShotResult applyShot(Cell cell) {
         CellState state = grid.get(cell.x).get(cell.y);
         if (state == CellState.SHIP) {
-            setCellAtPoint(cell, CellState.HIT_SHIP);
-            if (!hasShipCellAround(cell, true)) {
-                return ShotResult.SHIP_IS_DEAD;
+            setCell(cell, CellState.HIT_SHIP);
+            final Collection<Cell> ship = findShipByCell(cell);
+            if (isShipDead(ship)) {
+                markShipAsDead(ship);
             }
             return ShotResult.HIT;
         } else if (state == CellState.EMPTY) {
-            setCellAtPoint(cell, CellState.MISSED_SHOT);
+            setCell(cell, CellState.MISSED_SHOT);
         }
         return ShotResult.MISSED;
     }
 
-    private boolean hasShipCellAround(Cell cell, boolean checkAround) {
-        final Iterable<Cell> surroundPoints = cell.findNeighborsOnField(getSize());
-        for (Cell p : surroundPoints) {
-            final CellState state = getCellState(p);
-            if (state == CellState.SHIP) {
+    public boolean hasAfloatShip() {
+        for (java.util.List<CellState> row : grid) {
+            if (row.contains(CellState.SHIP)) {
                 return true;
-            } else if (state == CellState.HIT_SHIP && checkAround) {
-                if (hasShipCellAround(p, false)) {
-                    return true;
-                }
             }
+        }
+        return false;
+    }
+
+    private void markShipAsDead(Collection<Cell> ship) {
+        ship.stream().forEach(cell -> setCell(cell, CellState.DEAD_SHIP));
+    }
+
+    private boolean isShipDead(Collection<Cell> ship) {
+        return ship.stream().filter(c -> getCellOpenState(c) == CellState.SHIP).count() == 0;
+    }
+
+    private Collection<Cell> findShipByCell(Cell ship) {
+        final Collection<Cell> shipCells = new HashSet<>();
+        boolean shipPresentLeft = true;
+        boolean shipPresentRight = true;
+        boolean shipPresentUp = true;
+        boolean shipPresentDown = true;
+        int i = 0;
+        while (shipPresentLeft || shipPresentRight || shipPresentDown || shipPresentUp) {
+            if (shipPresentLeft) shipPresentLeft = putIfShipCell(ship.translate(new Point(-i, 0)), shipCells);
+            if (shipPresentRight) shipPresentRight = putIfShipCell(ship.translate(new Point(i, 0)), shipCells);
+            if (shipPresentDown) shipPresentDown = putIfShipCell(ship.translate(new Point(0, -i)), shipCells);
+            if (shipPresentUp) shipPresentUp = putIfShipCell(ship.translate(new Point(0, i)), shipCells);
+            i++;
+        }
+        return shipCells;
+    }
+
+    private boolean putIfShipCell(Cell cell, Collection<Cell> shipCells) {
+        if (cell.isInside(getSize()) && getCellOpenState(cell).isShip()) {
+            shipCells.add(cell);
+            return true;
         }
         return false;
     }
